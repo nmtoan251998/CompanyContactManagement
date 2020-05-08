2b8808a606353f3889cb2d9fe44b124263efafcc
ngoan98tv <ngoan98travinh@gmail.com>
2020-05-08 11:53:56 +0700

    check user role for edit and delete actions

--- ClientApp/src/components/UserItem.jsx
+++ ClientApp/src/components/UserItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   ListItem,
   ListItemText,
@@ -13,10 +13,12 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import UserDialog from "./UserDialog";
 import DeleteConfirmDialog from "./DeleteConfirmDialog";
+import { AppContext } from "../App";
 
 function UserItem({ user, refresh }) {
   const [showDel, setShowDel] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
+  const { user: currentUser } = useContext(AppContext);
 
   return (
     <>
@@ -32,7 +34,9 @@ function UserItem({ user, refresh }) {
               {user.department && user.department.name}
             </>
           }
-          secondary={`${user.age} ages, @${user.address}`}
+          secondary={`${user.role === 0 ? "Manager" : "Staff"}, ${
+            user.age
+          } ages, @${user.address}`}
         />
         <ListItemSecondaryAction>
           <IconButton href={`tel:${user.phone}`}>
@@ -41,12 +45,16 @@ function UserItem({ user, refresh }) {
           <IconButton href={`mailto:${user.email}`}>
             <EmailIcon />
           </IconButton>
-          <IconButton onClick={() => setShowEdit(true)}>
-            <EditIcon />
-          </IconButton>
-          <IconButton onClick={() => setShowDel(true)}>
-            <DeleteIcon />
-          </IconButton>
+          {currentUser.role === 0 && (
+            <>
+              <IconButton onClick={() => setShowEdit(true)}>
+                <EditIcon />
+              </IconButton>
+              <IconButton onClick={() => setShowDel(true)}>
+                <DeleteIcon />
+              </IconButton>
+            </>
+          )}
         </ListItemSecondaryAction>
       </ListItem>
       <UserDialog
