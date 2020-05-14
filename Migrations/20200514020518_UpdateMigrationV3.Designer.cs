ca7e72e896a0b7b72170ce74d1bb93763d5a7886
noat <nmtoan251998@gmail.com>
2020-05-14 09:06:51 +0700

    Migrate new model

--- Migrations/20200514020518_UpdateMigrationV3.Designer.cs
+++ Migrations/20200514020518_UpdateMigrationV3.Designer.cs
@@ -3,14 +3,16 @@ using CompanyContactManagment.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CompanyContactManagment.Migrations
 {
     [DbContext(typeof(CompanyContactContext))]
-    partial class CompanyContactContextModelSnapshot : ModelSnapshot
+    [Migration("20200514020518_UpdateMigrationV3")]
+    partial class UpdateMigrationV3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200514020518_UpdateMigrationV3.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompanyContactManagment.Migrations
+{
+    public partial class UpdateMigrationV3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessageChats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(nullable: false),
+                    IsSeen = table.Column<bool>(nullable: false),
+                    SenderId = table.Column<int>(nullable: false),
+                    ReceiverId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageChats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MessageChats_Users_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MessageChats_Users_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MessageChats_ReceiverId",
+                table: "MessageChats",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MessageChats_SenderId",
+                table: "MessageChats",
+                column: "SenderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageChats");
+        }
+    }
+}
